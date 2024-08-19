1649de43dd58ede8eff9e25f7b563187175a08c4
PerScholarSarah <rmandolin22@gmail.com>
2024-08-19 12:01:02 -0400

    refactored /img folder

---
+++ modules/game.mjs
@@ -0,0 +1,3 @@
+function loadPlants(){
+    const seedPackets = document.querySelectorAll(".seedpacket");
+}
--- modules/plant.mjs
+++ modules/plant.mjs
@@ -1,7 +1,9 @@
+import * as data from "plantData";
+
 class Plant{
     constructor(data){
         this.name = data.name;
-        this.image = `/img/${data.name}.webp`;
+        this.image = `/img/${data.img}`;
         this.cost = data.cost;
         this.recharge = data.recharge * 1000; //s->ms conversion
         this.health = data.health;
@@ -10,5 +12,11 @@ class Plant{
     }
     async doAction(msg="does action"){
         console.log(`${this.name}: msg`);
+    }
+}
+
+class Sunflower extends Plant{
+    constructor(){
+        super(data.sunflower)
     }
 }
